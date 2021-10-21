b6550604570e87487010ed047e6a881c5f364085
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-21 10:53:16 -0300

    add utils models

---
+++ src/main/java/com/lucaslevi/hourcontrol/model/Calendar.java
@@ -0,0 +1,12 @@
+package com.lucaslevi.hourcontrol.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Calendar {
+}
---
+++ src/main/java/com/lucaslevi/hourcontrol/model/DateTipe.java
@@ -0,0 +1,14 @@
+package com.lucaslevi.hourcontrol.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class DateTipe {
+    private Long id;
+    private String Description;
+}
