59958139c5d655ea674bc6ac60e1ad508419d32c
Lucas Levi <lucas.lsl12@gmail.com>
2021-10-26 11:12:35 -0300

    add swagger configuration

--- src/main/java/com/lucaslevi/hourcontrol/swagger/SwaggerConfig.java
+++ src/main/java/com/lucaslevi/hourcontrol/swagger/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.lucaslevi.hourcontrol.swagger;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -11,9 +12,12 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket apiAdmin(){
